e3795efc40e783f596ccebfff69647a40644b11a
quannk198 <73680367+quannk198@users.noreply.github.com>
2023-10-23 19:34:13 +0700

    Add files via upload
    card

---
+++ config.js
@@ -0,0 +1,13 @@
+const nameGirl = 'EmBe';
+const giftUrl = 'Happy Birthday';
+const eventName = 'Chúc Mừng Sinh Nhật EmBe';
+const titleCard = 'Chúc Mừng Sinh Nhật EmBe';
+const contentCard = 'Chúc Embe của anh sinh nhật vui vẻ tràn ngập niềm vu. Chúc embe luôn trẻ, xinh đẹp và luôn nở nụ cười trên môi nha. Mong điều đẹp nhất sẽ đến với em trong hôm nay và cả những ngày sau';
+
+// phần dưới dành cho các bạn biết code, nếu muốn chỉnh ảnh đơn giản với base64
+// Cần hỗ trợ hãy liên hệ: 
+// Mr-Nam http://facebook.com/nam.nodemy
+// Các bạn muốn học lập trình thì tham gia Nhóm zalo tự học lập trình nhé: https://zalo.me/g/yhdkef092
+const giftImage = '387329507_263138289555644_5232333900205526846_n.jpg';
+const base64 = '';
+const giftImageBase64 = "data:image/jpg;base64, " + base64;
